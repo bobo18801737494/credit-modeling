cbc2c1fcc855a19303e78e7afc0ea42f5ab72872
最踏实的人 <deerainw@gmail.com>
2019-01-18 13:38:38 +0800

    新增两个批处理文件

---
+++ lesson1/conda-install.bat
@@ -0,0 +1,6 @@
+REM 运行此文件，一次性用 conda 安装常用 Python 包(包括 jupyter 与 spyder)
+REM 安装过程中，可以按 Ctrl+C 终止
+
+conda install numpy pandas matplotlib seaborn cx_Oracle scikit-learn jupyter spyder
+
+pause
---
+++ lesson1/pip-install.bat
@@ -0,0 +1,6 @@
+REM 运行此文件，一次性用 pip 安装常用 Python 包(包括 jupyter 与 spyder)
+REM 安装过程中，可以按 Ctrl+C 终止
+
+pip install numpy pandas matplotlib seaborn cx_Oracle scikit-learn jupyter spyder
+
+pause
